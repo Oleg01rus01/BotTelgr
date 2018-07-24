2af43c1df427589b6f4c38a0a6a5f5c489aa9bfa
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 13:26:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict'
-
-var tg = require('telegram-node-bot')('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY')
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY')
  
-tg.router.
-    when(['ping'], 'PingController')
-
-tg.controller('PingController', ($) => {
-    tg.for('ping', () => {
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
         $.sendMessage('pong')
-    })
-}) 
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
