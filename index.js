8fb2fd1189b84a3d37a343bd99de1807429d2308
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 14:05:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 'use strict'
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY')
- 
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
+const Telegram = require('telegram-node-bot');
+var http = require('http');
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const tg = new Telegram.Telegram('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY');
 
-    get routes() {
-        return {
-            'pingCommand': 'pingHandler'
-        }
-    }
-}
+http.createServer(function (req, res) {
+    
+	class PingController extends TelegramBaseController {
+		/**
+		 * @param {Scope} $
+		 */
+		pingHandler($) {
+			$.sendMessage('pong')
+		}
 
-tg.router
-    .when(
-        new TextCommand('/ping', 'pingCommand'),
-        new PingController()
-    )
+		get routes() {
+			return {
+				'pingCommand': 'pingHandler'
+			}
+		}
+	}
+
+	tg.router
+		.when(
+			new TextCommand('/ping', 'pingCommand'),
+			new PingController()
+		)
+    
+}).listen(process.env.PORT || 8080);
