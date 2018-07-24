1e90551204cdc36c2ffc3a424713c7762b21a5ed
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 14:46:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 const Telegram = require('telegram-node-bot')
-var express = require("express")
-,app = express()
-const PORT = process.env.PORT || 5000
-app.set("PORT", PORT)
+var express = require('express')
+, app = express();
+const PORT = process.env.PORT || 5000;
+app.set("PORT", PORT);
 const TelegramBaseController = Telegram.TelegramBaseController;
 const TextCommand = Telegram.TextCommand;
 const tg = new Telegram.Telegram('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY'); 
