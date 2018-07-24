ad3d7c474254824cf57a2f96b72489d1fe423c3d
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 15:40:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const Telegram = require('telegram-node-bot')
 var app = require('express')();
 
 var listener = app.listen(port: process.env.PORT || 3000 , function(){
-    const TelegramBaseController = Telegram.TelegramBaseController;
+	const TelegramBaseController = Telegram.TelegramBaseController;
 	const TextCommand = Telegram.TextCommand;
 	const tg = new Telegram.Telegram('644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY'); 
 
 
-	class PingController extends TelegramBaseController {
-		/**
-		 * @param {Scope} $
-		 */
-		pingHandler($) {
-			$.sendMessage('pong')
-		}
+		class PingController extends TelegramBaseController {
+			/**
+			 * @param {Scope} $
+			 */
+			pingHandler($) {
+				$.sendMessage('pong')
+			}
 
-		get routes() {
-			return {
-				'pingCommand': 'pingHandler'
+			get routes() {
+				return {
+					'pingCommand': 'pingHandler'
+				}
 			}
 		}
-	}
-	
+		
 
 	tg.router
 		.when(
 			new TextCommand('/ping', 'pingCommand'),
 			new PingController()
-		)
+		);
 });
 
 
