5a4f96788ddbe1555040f9f1d47cb738b920fccd
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 15:27:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const requestHandler = (request, response) => {
 }
 const server = http.createServer(requestHandler)
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+server.connection({
+    port: process.env.PORT || 3000 
 });
 
 const TelegramBaseController = Telegram.TelegramBaseController;
