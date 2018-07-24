fa3fa044e92565e6080fe8af5cd13eb315f7de39
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 15:12:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict'
 const Telegram = require('telegram-node-bot')
-var http = require('http');
-var express = require("express");
 
-var app = express();
-var port = process.env.PORT || 3000;
-http.createServer(app).listen(port, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
 const TelegramBaseController = Telegram.TelegramBaseController;
