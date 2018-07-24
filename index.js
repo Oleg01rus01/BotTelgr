18e2f5fafa8188199c89e48d6c69c5fa369f053b
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-24 15:19:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 const Telegram = require('telegram-node-bot')
-
+const http = require('http');
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+http.createServer.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
